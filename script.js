0d8add1ec19a7a62fcd0f926cc3da9568c77b69d
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-09 11:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+
+  // Convert input strings to Date objects
+  const firstDate = new Date(date1);
+  const secondDate = new Date(date2);
+
+  const diffInMilliseconds = secondDate - firstDate;
+
+  const diffInDays = diffInMilliseconds / oneDay;
+
+  return diffInDays;
+	 
 };
 
 // Do not change the code below.
